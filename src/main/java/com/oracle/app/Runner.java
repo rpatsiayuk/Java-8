de68f56e2ce00e2bdf07837ed3a1aec36eaf6225
Raman Patsiayuk <raman_patsiayuk@epam.com>
2017-12-01 17:28:33 +0300

    Added test dependencies

--- src/main/java/com/oracle/app/Runner.java
+++ src/main/java/com/oracle/app/Runner.java
@@ -1,8 +1,14 @@
 package com.oracle.app;
 
 import com.oracle.model.Student;
+import org.junit.Assert;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.toList;
 
 public class Runner {
 
@@ -12,9 +18,23 @@ public class Runner {
         System.out.println(getNumberOfStudentsOldVersion(students));
         System.out.println(getNumberOfStudentsJava8Version(students));
 
+        List<Integer> numbers = asList(5, 2, 3, 4);
+
+        List<Integer> stillOrdered = numbers.stream()
+                .map(x -> x + 1)
+                .collect(toList());
+        stillOrdered.forEach(System.out::print);
+        Assert.assertEquals(asList(6, 3, 4, 5), stillOrdered);
+
+        Set<Integer> unordered = new HashSet<>(numbers);
+        List<Integer> stillUnordered = unordered.stream()
+                .map(x -> x + 1)
+                .collect(toList());
+        stillUnordered.forEach(System.out::print);
+        Assert.assertEquals(asList(6, 3, 4, 5), stillUnordered);
     }
 
-    private static long getNumberOfStudentsOldVersion(List<Student> students){
+    private static long getNumberOfStudentsOldVersion(List<Student> students) {
         long count = 0;
         for (Student st : students) {
             if (st.getAge() >= 20) {
@@ -24,9 +44,9 @@ public class Runner {
         return count;
     }
 
-    private static long getNumberOfStudentsJava8Version(List<Student> students){
+    private static long getNumberOfStudentsJava8Version(List<Student> students) {
         return students.stream()
-               .filter(st-> st.getAge()>=20)
-               .count();
+                .filter(st -> st.getAge() >= 20)
+                .count();
     }
 }
